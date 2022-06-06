648eb4acf8eb9dcae802cec93b0628f0fdb3b455
Fred Higgins <higgins.fred.s@gmail.com>
2022-06-06 09:44:08 -0700

    commit tree 1e3333f61dbb28e8e3cc64b1cb634f7eabb6430f

--- db/migrate/20210506153121_create_passengers.rb
+++ db/migrate/20210506153121_create_passengers.rb
@@ -1,6 +1,8 @@
 class CreatePassengers < ActiveRecord::Migration[6.1]
   def change
     create_table :passengers do |t|
+      t.integer "ride_id"
+      t.integer "taxi_id"
 
       t.timestamps
     end
